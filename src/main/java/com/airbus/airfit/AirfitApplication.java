f6b629c1250a14975562b9cc9806ab36a783764e
ganout <morgan.bourdillel@gmail.com>
2020-09-22 10:30:53 +0200

    init project

---
+++ src/main/java/com/airbus/airfit/AirfitApplication.java
@@ -0,0 +1,13 @@
+package com.airbus.airfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirfitApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/airbus/airfit/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.airbus.airfit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String getIndex() {
+
+        return "index";
+    }
+}

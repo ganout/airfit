18c9827f1424ddaea8e6cc91a53f1015015b5888
ganout <morgan.bourdillel@gmail.com>
2020-09-29 14:58:23 +0200

    background changed

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,8 +3,3 @@ $(document).ready(function(){
 });
 
 $('.carousel.carousel-slider').carousel({fullWidth: true});
-
-$('.fixed-action-btn').openFAB();
-$('.fixed-action-btn').closeFAB();
-$('.fixed-action-btn.toolbar').openToolbar();
-$('.fixed-action-btn.toolbar').closeToolbar();
